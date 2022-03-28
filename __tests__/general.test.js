379ba960f8bbae52683b550a4001984d5892205a
rallosdrake <jimbobbarlow74@gmail.com>
2022-03-28 17:04:28 +0100

    Console.logs removed

--- __tests__/general.test.js
+++ __tests__/general.test.js
@@ -15,13 +15,11 @@ describe(`GET/api/topics`, () => {
   test(`200: responds with an array of all topics`, async () => {
     const res = await request(app).get(`/api/topics`).expect(200);
     return res.body.topics.forEach((topic) => {
-      console.log(res.body);
       expect(topic).toMatchObject(topicObj);
     });
   });
   test.only(`404: invalid path results in a 404 error`, async () => {
     const res = await request(app).get(`/api/not_a_path`).expect(404);
-    console.log(res.body, "!");
     return expect(res.body.msg).toBe("Route not found");
   });
 });
--- controllers.js
+++ controllers.js
@@ -4,7 +4,6 @@ const { getTopics } = require(`./model`);
 exports.getAllTopics = async (res, req, next) => {
   try {
     const topics = await getTopics();
-    console.log();
     res.send({ topics });
   } catch (err) {
     next(err);
