429d76c6a80eb2063a8f1c597c83daa80b119793
rallosdrake <jimbobbarlow74@gmail.com>
2022-03-29 12:18:37 +0100

    added semi colons at end of SQL

--- MODELS/article.model.js
+++ MODELS/article.model.js
@@ -15,7 +15,7 @@ exports.changeArticleById = (article_id, inc_votes) => {
   if (inc_votes) {
     return db
       .query(
-        `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`,
+        `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;`,
         [inc_votes, article_id]
       )
       .then((result) => {
--- MODELS/topic.model.js
+++ MODELS/topic.model.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
 exports.getTopics = () => {
-  return db.query("SELECT * FROM topics").then((result) => {
+  return db.query("SELECT * FROM topics;").then((result) => {
     return result.rows;
   });
 };
