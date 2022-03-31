4ed171b85b78972ccbcef1b4b5af0a27218a1e2e
rallosdrake <jimbobbarlow74@gmail.com>
2022-03-31 20:29:32 +0100

    username endpoint added

--- MODELS/users.models.js
+++ MODELS/users.models.js
@@ -5,3 +5,15 @@ exports.getUsers = () => {
     return result.rows;
   });
 };
+
+exports.getUsersByUsername = (username) => {
+  return db
+    .query(`SELECT * FROM users WHERE username = $1;`, [username])
+    .then((result) => {
+      if (result.rows.length) {
+        return result.rows[0];
+      } else {
+      }
+      return Promise.reject({ status: 404, msg: "Invalid username" });
+    });
+};
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,9 +1,20 @@
-const { getUsers } = require(`../MODELS/users.models`);
+const { getUsers, getUsersByUsername } = require(`../MODELS/users.models`);
 
 exports.getAllUsers = (req, res, next) => {
   getUsers()
     .then((result) => {
       res.send({ users: result });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getAllUsersByUsername = (req, res, next) => {
+  const { username } = req.params;
+  getUsersByUsername(username)
+    .then((result) => {
+      res.send({ user: result });
     })
     .catch((err) => {
       next(err);
